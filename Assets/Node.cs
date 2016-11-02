6c95230107ac9e4d99a5ea03b6a21c28ae4fdaca
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-11-02 15:00:33 +0100

    added crap // added lsystem

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -1,10 +1,17 @@
 ﻿
-namespace L_System
-{
-    [System.Serializable]
-    public class Node
-    {
-        public bool use = false;
+using UnityEngine;
 
+[System.Serializable]
+public class Node
+{
+    public Vector3 position;
+    public Vector3 angle;
+    public float length;
+
+    public Node(Vector3 point, Vector3 angle, float len)
+    {
+        this.position = point;
+        this.angle = angle;
+        length = len;
     }
 }
